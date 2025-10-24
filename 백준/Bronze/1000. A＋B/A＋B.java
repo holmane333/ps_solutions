a3ada060e2f0477e4e5b111bfd37d37496bf3702
Minuk Kim <holmane33@gmail.com>
2025-10-24 18:33:05 +0900

    [Bronze V] Title: A+B, Time: 108 ms, Memory: 14620 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Integer a = Integer.parseInt(st.nextToken());
+        Integer b = Integer.parseInt(st.nextToken());
+        bw.write(String.format("%d\n", a + b));
+
+        bw.flush();
+        bw.close();
+    }
+}
