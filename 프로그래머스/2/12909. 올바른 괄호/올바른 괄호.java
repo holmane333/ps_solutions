e53c3a8ff6b3ef0b5b51f1f830588a8a20a72630
Minuk Kim <holmane33@gmail.com>
2025-10-26 13:43:38 +0900

    [level 2] Title: 올바른 괄호, Time: 6.18 ms, Memory: 53.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,18 @@
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        int stack = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            if (s.charAt(i) == '(') ++stack;
+            else {
+                if (stack == 0) {
+                    answer = false;
+                    break;
+                }
+                --stack;
+            }
+        }
+        if (stack != 0) answer = false;
+        return answer;
+    }
+}
