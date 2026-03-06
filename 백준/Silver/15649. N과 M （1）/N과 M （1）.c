9be5cc8b19a3bbf10b91e746762bfad4c5fc9e45
Minuk Kim <holmane33@gmail.com>
2026-03-06 10:19:35 +0900

    [Silver III] Title: N과 M (1), Time: 28 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+#pragma warning(disable:4996)
+
+int count = 0;
+int mat1[10], mat2[10];
+
+
+void z(int n, int m) {
+	if (count == m) {
+		for (int i = 0; i < m; i++) {
+			printf("%d ", mat1[i]);
+		}
+		puts("");
+	}
+	else {
+		for (int i = 1; i <= n; i++) {
+			if (mat2[i]) continue;
+			else {
+				mat1[count++] = i;
+				mat2[i] = 1;
+				z(n, m);
+				mat1[--count] = 0;
+				mat2[i] = 0;
+			}
+		}
+	}
+}
+
+int main() {
+	int n, m;
+	scanf("%d %d", &n, &m);
+	z(n, m);
+	return 0;
+}
