3ddec4b0a900439503e851a3c449dc032384e4bc
Minuk Kim <holmane33@gmail.com>
2026-03-14 20:50:17 +0900

    [Platinum V] Title: 속타는 저녁 메뉴, Time: 456 ms, Memory: 283900 KB -BaekjoonHub

---
+++ 백준/Platinum/11585. 속타는 저녁 메뉴/속타는 저녁 메뉴.py
@@ -0,0 +1,34 @@
+import math
+
+def failure(str1, str2, ary):
+    global res
+    index = 0
+    for i in range(len(str2)):
+        while (index > 0 and str2[i] != str1[index]): 
+            index = ary[index - 1]
+        
+        if (str2[i] == str1[index]):
+            if (index == len(str1) - 1):
+                res += 1
+                index = ary[index]
+            else:
+                index += 1
+
+N = int(input())
+str1, str2 = list(input().split()), list(input().split())
+for i in range(N - 1):
+    str2.append(str2[i])
+ary = [0 for i in range(len(str1) + 1)]
+index = 0
+for i in range(1, len(str1)):
+    while (index > 0 and str1[i] != str1[index]): 
+        index = ary[index - 1]
+    if (str1[i] == str1[index]): 
+        index += 1
+        ary[i] = index
+
+res = 0
+failure(str1, str2, ary)
+
+g = math.gcd(res, N)
+print(f"{res // g}/{N // g}")
